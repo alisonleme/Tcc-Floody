3fafd7a8f18548efbee28403473d824a06935ebb
alison.lemes.p9 <alison.lemes.p9@gmail.com>
2025-07-24 23:20:30 +0000

    pronto

--- src/pages/SobreNos.jsx
+++ src/pages/SobreNos.jsx
@@ -93,7 +93,6 @@ export default function SobreNos({ darkMode, toggleTheme }) {
     sectionsRef.current[key] = el;
   };
 
-  // Classe padrão dos botões (igual à página ComoFunciona)
   const buttonClasses =
     "px-6 py-3 mt-4 font-semibold rounded-full shadow-lg transition-all duration-500 transform hover:scale-105 hover:shadow-2xl animated-button";
 
@@ -105,7 +104,7 @@ export default function SobreNos({ darkMode, toggleTheme }) {
           : "bg-gradient-to-br from-[#b3ddfe] to-[#71b7e6] text-gray-900"
       }`}
     >
-      {/* Estilo animado igual da página ComoFunciona */}
+      {/* Estilo animado dos botões */}
       <style>{`
         @keyframes shimmer {
           0% { background-position: 0% 50%; }
@@ -186,16 +185,27 @@ export default function SobreNos({ darkMode, toggleTheme }) {
             <div className="flex justify-center gap-4 mt-4">
               {/* LinkedIn */}
               <a href={dev.linkedin} target="_blank" rel="noopener noreferrer" className="hover:scale-125 transition-transform">
-                {/* SVG igual ao anterior */}
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50" width="28" height="28" fill="currentColor"
+                  className={`${darkMode ? "text-white" : "text-gray-800"} hover:text-blue-500`}>
+                  <path d="M41,4H9C6.8,4,5,5.8,5,8v34c0,2.2,1.8,4,4,4h32c2.2,0,4-1.8,4-4V8C45,5.8,43.2,4,41,4z M17,39h-5V20h5V39z M14.5,17.7 c-1.7,0-3-1.3-3-3s1.3-3,3-3c1.7,0,3,1.3,3,3S16.2,17.7,14.5,17.7z M40,39h-5v-9.5c0-2.3-0.8-3.8-2.7-3.8 c-1.5,0-2.3,1-2.7,2c-0.1,0.3-0.1,0.7-0.1,1.1V39h-5c0,0,0.1-16,0-19h5v2.7c0.7-1,1.9-2.5,4.6-2.5c3.3,0,5.8,2.2,5.8,6.8V39z"></path>
+                </svg>
               </a>
+
               {/* GitHub */}
               <a href={dev.github} target="_blank" rel="noopener noreferrer" className="hover:scale-125 transition-transform">
-                {/* SVG igual ao anterior */}
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30" width="28" height="28" fill="currentColor"
+                  className={`${darkMode ? "text-white" : "text-gray-800"} hover:text-purple-500`}>
+                  <path d="M15,3C8.373,3,3,8.373,3,15c0,5.623,3.872,10.328,9.092,11.63C12.036,26.468,12,26.28,12,26.047v-2.051 c-0.487,0-1.303,0-1.508,0c-0.821,0-1.551-0.353-1.905-1.009c-0.393-0.729-0.461-1.844-1.435-2.526 c-0.289-0.227-0.069-0.486,0.264-0.451c0.615,0.174,1.125,0.596,1.605,1.222c0.478,0.627,0.703,0.769,1.596,0.769 c0.433,0,1.081-0.025,1.691-0.121c0.328-0.833,0.895-1.6,1.588-1.962c-3.996-0.411-5.903-2.399-5.903-5.098 c0-1.162,0.495-2.286,1.336-3.233C9.053,10.647,8.706,8.73,9.435,8c1.798,0,2.885,1.166,3.146,1.481C13.477,9.174,14.461,9,15.495,9 c1.036,0,2.024,0.174,2.922,0.483C18.675,9.17,19.763,8,21.565,8c0.732,0.731,0.381,2.656,0.102,3.594 c0.836,0.945,1.328,2.066,1.328,3.226c0,2.697-1.904,4.684-5.894,5.097C18.199,20.49,19,22.1,19,23.313v2.734 c0,0.104-0.023,0.179-0.035,0.268C23.641,24.676,27,20.236,27,15C27,8.373,21.627,3,15,3z"></path>
+                </svg>
               </a>
+
               {/* Instagram (se existir) */}
               {dev.instagram && (
                 <a href={dev.instagram} target="_blank" rel="noopener noreferrer" className="hover:scale-125 transition-transform">
-                  {/* SVG igual ao anterior */}
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50" width="28" height="28" fill="currentColor"
+                    className={`${darkMode ? "text-white" : "text-gray-800"} hover:text-pink-500`}>
+                    <path d="M 16 3 C 8.8324839 3 3 8.8324839 3 16 L 3 34 C 3 41.167516 8.8324839 47 16 47 L 34 47 C 41.167516 47 47 41.167516 47 34 L 47 16 C 47 8.8324839 41.167516 3 34 3 L 16 3 z M 16 5 L 34 5 C 40.086484 5 45 9.9135161 45 16 L 45 34 C 45 40.086484 40.086484 45 34 45 L 16 45 C 9.9135161 45 5 40.086484 5 34 L 5 16 C 5 9.9135161 9.9135161 5 16 5 z M 37 11 A 2 2 0 0 0 35 13 A 2 2 0 0 0 37 15 A 2 2 0 0 0 39 13 A 2 2 0 0 0 37 11 z M 25 14 C 18.936712 14 14 18.936712 14 25 C 14 31.063288 18.936712 36 25 36 C 31.063288 36 36 31.063288 36 25 C 36 18.936712 31.063288 14 25 14 z M 25 16 C 29.982407 16 34 20.017593 34 25 C 34 29.982407 29.982407 34 25 34 C 20.017593 34 16 29.982407 16 25 C 16 20.017593 20.017593 16 25 16 z"></path>
+                  </svg>
                 </a>
               )}
             </div>
